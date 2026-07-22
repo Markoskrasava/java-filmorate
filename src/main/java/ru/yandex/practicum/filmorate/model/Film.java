73f1af9730d46c83e6ebabde453e151ca68c76f9
Markos <markbyckov8@gmail.com>
2026-07-22 19:12:10 +0300

    Добавить контроллеры

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Film.
@@ -14,4 +15,6 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
+    private Mpa mpa;
+    private List<Genre> genres;
 }
