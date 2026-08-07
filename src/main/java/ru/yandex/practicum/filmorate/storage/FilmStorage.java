cd5daa67165f406b88b0321738c26d14e209da13
Markoskrasava <markbyckov8@gmail.com>
2026-08-08 02:01:23 +0300

    Исправлены пробелы

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,11 +6,19 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Collection<Film> findAll();
+
     Film create(Film film);
+
     Film update(Film newFilm);
+
     Optional<Film> getFilmById(Long id);
+
     Film addLike(Long id, Long userId);
+
     Film deleteLike(Long id, Long userId);
+
     Collection<Film> getMostLikebleFilms(long count);
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -7,13 +7,23 @@ import java.util.Map;
 import java.util.Optional;
 
 public interface UserStorage {
+
     Collection<User> findAll();
+
     User create(User user);
+
     User update(User newUser);
+
     Optional<User> getUserById(Long id);
+
     User addNewFriend(Long id, Long friendId);
+
     User deleteFriend(Long id, Long friendId);
+
     Collection<User> getAllFriendsByUser(Long id);
+
     Collection<User> getAllGeneralFriends(Long id, Long otherId);
+
     Map<Long, User> getUsers();
+    
 }
