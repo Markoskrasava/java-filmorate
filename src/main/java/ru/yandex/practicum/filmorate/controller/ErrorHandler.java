f79ee081e4e352a4c5896c96b438bf82451a36e1
Markoskrasava <markbyckov8@gmail.com>
2026-08-08 13:51:46 +0300

    Исправить обработку исключений

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -23,7 +23,7 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleOther(Throwable e) {
+    public ErrorResponse handleOther(RuntimeException e) {
         return new ErrorResponse("Произошла непредвиденная ошибка.");
     }
 
