486bbdba5c29d890495c6164cb26c50ba13e2049
Markoskrasava <markbyckov8@gmail.com>
2026-09-06 20:16:37 +0300

    Удалить импорты

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.InternalServerException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 import ru.yandex.practicum.filmorate.storage.mappers.UserRowMapper;
